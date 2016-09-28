f41c905316ee0dc4dd8e1259bbcc0b8ebb353d32
DiegoSCI <diego.jfreitas@hotmail.com>
2016-09-28 19:45:33 -0400

    Versão 1.1

--- script1.sh
+++ script1.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Script para modificar o nome do computador e buscar endereço ip atráves do DHCP.
-#
+# William Martin & Diego Justin
 dhclient eth0 -v 
 
 whoami > /etc/hostname
--- script3.sh
+++ script3.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 #Este script vai demonstrar a versão do sistema operacional e arquitetura.
+# William Martin & Diego Justin
 cat /etc/issue.net 
 uname -m 
