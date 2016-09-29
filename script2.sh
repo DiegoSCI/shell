eda685a39557bbb0b2bb06072aa15d9a28353e59
DiegoSCI <diego.jfreitas@hotmail.com>
2016-09-28 21:21:16 -0300

    Update script2.sh

--- script2.sh
+++ script2.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #Diego Justin
+#Script para a troca de porta do SSHd
 apt-get update ; apt-get install openssh-server -y 
 sed -i "s/Port 22/Port 10000/g" /etc/ssh/sshd_config
 invoke-rc.d ssh restart
